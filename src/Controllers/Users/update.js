7559bd9ed81d15d11509476862ae63b56f32360d
NathanielPerez <nathaniel.perez@corebiz.ag>
2022-04-22 03:59:50 -0500

    update complete

--- src/Controllers/Users/update.js
+++ src/Controllers/Users/update.js
@@ -1,11 +1,38 @@
 const {returnSuccessInfo} = require('../../functions/returnSuccessInfo')
 const {returnError} = require('../../functions/returnError')
 const { response } = require('express')
+const {Users} = require('../../Models/users')
+const bcrypt = require('bcryptjs')
 
 
-const UpdateUser = (req, res = response) => {
+const UpdateUser = async (req, res = response) => {
     try {
-        res.status(200).json(returnSuccessInfo('update user', {user: 1}))
+        const { uid } = req
+        const {username, name, lastname, password, birthday, gender, height} = req.body
+
+        const userFind = await Users.findByPk(uid)
+        console.log( uid, username, name, lastname, password, birthday, gender, height, userFind )
+        const validationPassword = bcrypt.compareSync( password, userFind.password )
+        if( !validationPassword ) {
+            const salt = bcrypt.genSaltSync()
+            userFind.password = bcrypt.hashSync(password, salt)
+        }
+
+        userFind.username = username
+        userFind.name = name
+        userFind.lastname = lastname
+        userFind.birthday = birthday
+        userFind.gender = gender
+        userFind.height = height
+
+        userFind.save()
+
+        const user = {...userFind?.dataValues}
+
+        delete user.id
+        delete user.password
+
+        res.status(200).json(returnSuccessInfo('update user', {user}))
     } catch (e) {
         res.status(500).json(returnError('update user', e))
     }
--- src/Routes/User/index.js
+++ src/Routes/User/index.js
@@ -34,7 +34,19 @@ router.post('/', [
 ],AddUser )
 
 router.put('/',[
-    validateJWT
+    validateJWT,
+    check('email', 'Email invalid').isEmail(),
+    check('email', 'Email is required').not().isEmpty(),
+    check('username', 'Username is required').not().isEmpty(),
+    check('name', 'Name is required').not().isEmpty(),
+    check('lastname', 'Lastname is required').not().isEmpty(),
+    check('password', 'Password is required').not().isEmpty(),
+    check('password', 'Password minlength 6').isLength({ min: 6 }),
+    check('gender', 'Gender is required').not().isEmpty(),
+    check('birthday', 'Birthday is required').not().isEmpty(),
+    check('birthday', 'Birthday is not a date').isDate(),
+    check('height', 'Height is required').isDecimal(),
+    infoValidate,
 ], UpdateUser )
 
 router.delete('/',[
